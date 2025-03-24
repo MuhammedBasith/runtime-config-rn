8de2d1f59809825d0c7df88ffa2648236d3ffa64
MuhammedBasith <iambasith123@gmail.com>
2025-03-24 08:27:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Super App Example
+
+Follow these steps to set up the project on your local machine.
+
+### Prerequisites
+
+- Make sure you have the latest version of `node` and `yarn` installed on your system. Please note that this project uses `yarn@3.x.x`, and not the classic version.
+
+### Installation
+
+1. Clone the repository:
+
+   ```sh
+   git clone
+   ```
+
+2. Install the dependencies (including pods):
+
+   ```sh
+   yarn bootstrap
+   ```
+
+### Running the app
+
+1. Start the dev servers
+
+   ```sh
+   yarn start
+   ```
+
+2. Run the host-app on `ios` or `android`:
+
+   ```sh
+   yarn run:host-app:ios
+   # or
+   yarn run:host-app:android
+   ```
---
+++ packages/host-app/index.js
@@ -0,0 +1,73 @@
+import {AppRegistry, Platform, LogBox} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+import {ScriptManager, Federated} from '@callstack/repack/client';
+
+LogBox.ignoreLogs(['Warning: ...']);
+
+// MiniApp Remote config
+const appUrl = 'https://storage.googleapis.com/mini_app';
+const updateFile = 'update.json';
+let latestVersion = '1.0.0';
+
+// Function to fetch the latest version from the API
+const fetchLatestVersion = async () => {
+  try {
+    const updateResponse = await fetch(
+      `${appUrl}/${updateFile}?t=${new Date().getTime()}`,
+    );
+    if (!updateResponse.ok) {
+      throw new Error('Failed to fetch version data');
+    }
+
+    const updateData = await updateResponse.json();
+    // Update the latestVersion from the JSON
+    latestVersion = updateData.latest_version;
+    console.log('Fetched latest version:', latestVersion);
+  } catch (e) {
+    console.error('Failed to fetch the latest version:', e);
+  }
+};
+
+// Main function to initialize ScriptManager after fetching version
+const initializeScriptManager = async () => {
+  // Fetch the latest version
+  await fetchLatestVersion();
+
+  // Setup the URL resolver with the fetched latest version
+  const resolveURL = Federated.createURLResolver({
+    containers: {
+      //MiniApp: 'http://localhost:9000/[name][ext]',
+      MiniApp: `${appUrl}/${latestVersion}/${Platform.OS}/[name][ext]`,
+      news: `https://github.com/callstack/news-mini-app-showcase/releases/download/news-${Platform.OS}%400.0.1/[name][ext]`,
+    },
+  });
+
+  // Add the resolver to ScriptManager
+  ScriptManager.shared.addResolver(async (scriptId, caller) => {
+    let url;
+    if (caller === 'main') {
+      url = Script.getDevServerURL(scriptId);
+    } else {
+      url = resolveURL(scriptId, caller);
+    }
+
+    if (!url) {
+      return undefined;
+    }
+    console.log('Resolved URL:', url);
+
+    return {
+      url,
+      cache: false, // For development
+      query: {
+        platform: Platform.OS,
+      },
+    };
+  });
+};
+
+// Initialize the ScriptManager and register the App
+initializeScriptManager();
+AppRegistry.registerComponent(appName, () => App);
